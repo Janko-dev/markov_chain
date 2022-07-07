cad868c0f069d50dbaf8e913d830e4b59be1e9af
jan baljan <jan.baljan@hotmail.com>
2022-07-08 00:36:36 +0200

    fixed segfault issue by reducing malloc calls

--- Sentence_generation/main.c
+++ Sentence_generation/main.c
@@ -12,11 +12,12 @@
 // in
 // tree
 
-#define WORD_SIZE 50
+#define BUF_SIZE 2048
+#define WORD_LEN 40
 
 #define rnd_float ((rand() % RAND_MAX)/(float)RAND_MAX)
 
-int toIndex(char* word, char** wset, int set_index){
+int toIndex(char* word, char wset[BUF_SIZE][WORD_LEN], int set_index){
     for (int i = 0; i < set_index; i++){
         if (strcmp(wset[i], word) == 0){
             return i;
@@ -30,24 +31,37 @@ int main(void){
     srand(time(NULL));
 
     // Input string
-    char text[] = "De grote tafel is zacht van binnen, maar ik ben dat ook, dus eigenlijk is dit wel grappig.";
+    FILE* f = fopen("test.txt", "r");
+    char text[0xffff];
+    size_t len = fread(text, sizeof(char), 0xffff, f);
+    text[len] = '\0';
+    //printf("TEST: %s\n", text);
+    //char text[] = "Both versions convey a topic; it's pretty easy to predict that the paragraph will be about epidemiological evidence, but only the second version establishes an argumentative point and puts it in context. The paragraph doesn't just describe the epidemiological evidence; it shows how epidemiology is telling the same story as etiology. Similarly, while Version A doesn't relate to anything in particular, Version B immediately suggests that the prior paragraph addresses the biological pathway (i.e. etiology) of a disease and that the new paragraph will bolster the emerging hypothesis with a different kind of evidence. As a reader, it's easy to keep track of how the paragraph about cells and chemicals and such relates to the paragraph about populations in different places.";
 
     // string buffer
-    char* buffer[WORD_SIZE];
+    char buffer[BUF_SIZE][WORD_LEN];
     int buf_index = 0;
     // set of words (no duplicates)
-    char* word_set[WORD_SIZE];
+    char word_set[BUF_SIZE][WORD_LEN];
     int set_index = 0;
 
     // splitting the input string
     char* token;
     token = strtok(text, " ");
-    while (token != NULL){
-        size_t word_len = strlen(token);
+
+    while (token != NULL && buf_index < BUF_SIZE){
+        //size_t word_len = strlen(token);
+        
+        //printf("TEST: %s - %d\tbuf_index: %d\tset_index: %d\n", token, word_len, buf_index, set_index);
 
         // malloc and copy token to string buffer
-        buffer[buf_index] = malloc(sizeof(char) * word_len);
+        //buffer[buf_index] = (char*)malloc(sizeof(char) * word_len);
+        // if (buffer[buf_index] == NULL){
+        //     fprintf(stderr, "Error: can't allocate enough resources for the string buffer\n");
+        //     exit(1);
+        // }
         strcpy(buffer[buf_index], token);
+        //printf("TEST: %s\n", buffer[buf_index]);
         buf_index++;
 
         // check if token already in word set
@@ -61,9 +75,14 @@ int main(void){
             token = strtok(NULL, " ");
             continue;
         }
-
+        
         // if not found in word set, malloc, copy and add word to set
-        word_set[set_index] = malloc(sizeof(char) * word_len);
+        //word_set[set_index] = (char*)malloc(sizeof(char) * word_len);
+        //printf("LEN: %d\n", sizeof(char) * word_len);
+        // if (word_set[set_index] == NULL){
+        //     fprintf(stderr, "Error: can't allocate enough resources for the word set\n");
+        //     exit(1);
+        // }
         // TODO: segfault after +-16 mallocs, needs fixing
         strcpy(word_set[set_index], token);
         set_index++;
@@ -72,17 +91,19 @@ int main(void){
     }
 
     // debug word set
-    printf("Word_set: ");
-    for (int i = 0; i < set_index; i++) printf("%s ", word_set[i]); 
+    // printf("Word_set: ");
+    // for (int i = 0; i < set_index; i++) printf("%s ", word_set[i]); 
 
-    // debug string buffer
-    printf("\nBuffer: ");
-    for (int i = 0; i < buf_index; i++) printf("%s ", buffer[i]); 
-    printf("\n");
+    // // debug string buffer
+    // printf("\n\nBuffer: ");
+    // for (int i = 0; i < buf_index; i++) printf("%s ", buffer[i]); 
+    // printf("\n\n");
     
     // state transition matrix
-    float trans_matrix[WORD_SIZE][WORD_SIZE] = {{0}};
-    
+    float** trans_matrix = (float**)malloc(set_index * sizeof(float*));
+    for (int i = 0; i < set_index; i++) trans_matrix[i] = (float*)calloc(set_index, sizeof(float));
+    //[WORD_SIZE][WORD_SIZE] = {{0}};
+
     // adjusting weights in transition matrix
     for (int i = 0; i < buf_index-1; i++){
         trans_matrix
@@ -91,30 +112,31 @@ int main(void){
     }
 
     // string buffer
-    for (int i = 0; i < buf_index; i++) free(buffer[i]);
+    // for (int i = 0; i < buf_index; i++) free(buffer[i]);
+    // free(buffer);
 
     // evaluate weights
-    for (int i = 0; i < WORD_SIZE; i++){
+    for (int i = 0; i < set_index; i++){
         float sum = 0.0f;
-        for (int j = 0; j < WORD_SIZE; j++){
+        for (int j = 0; j < set_index; j++){
             sum += trans_matrix[i][j];
         }
         if (sum == 0) continue; 
-        for (int j = 0; j < WORD_SIZE; j++){
+        for (int j = 0; j < set_index; j++){
             trans_matrix[i][j] = trans_matrix[i][j]/sum;
         }
     }
 
     // debug print transition matrix
-    for (int i = 0; i < set_index; i++) printf("    %2d", i);
-    printf("\n");
-    for (int i = 0; i < set_index; i++){
-        printf("%2d| ", i);
-        for (int j = 0; j < set_index; j++){
-            printf("%2.2f  ", trans_matrix[i][j]);
-        }
-        printf("\n");
-    }
+    // for (int i = 0; i < set_index; i++) printf("    %2d", i);
+    // printf("\n");
+    // for (int i = 0; i < set_index; i++){
+    //     printf("%2d| ", i);
+    //     for (int j = 0; j < set_index; j++){
+    //         printf("%2.2f  ", trans_matrix[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
     // Generate sentence based on markov chain model
     int done = 0;
@@ -137,8 +159,9 @@ int main(void){
         if (!selected) done = 1;
     }
 
-    // clean word set
-    for (int i = 0; i < set_index; i++) free(word_set[i]);
+    // // clean word set
+    // for (int i = 0; i < set_index; i++) free(word_set[i]);
+    // free(word_set);
 
     return 0;
 }
